dfc519a12b2c110e265bb2cc37ea7f61c7ffaadd
Naksh-Rathore <naksh.rathore786@gmail.com>
2025-10-27 17:55:55 -0500

    Add makefile

--- SDLStart/main.cpp
+++ SDLStart/main.cpp
@@ -10,20 +10,22 @@ int main(int argc, char* argv[]) {
 	}
 
 	SDL_Window* window = SDL_CreateWindow("SDL3", 800, 600, SDL_WINDOW_RESIZABLE);
-	
+
 	if (window == nullptr) {
 		std::cerr << "Window pointer was null: " << SDL_GetError() << "\n";
 		return EXIT_FAILURE;
 	}
-	
+
 	SDL_Event event;
 	bool isRunning = true;
 
 	while (isRunning) {
 		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_EVENT_QUIT) {
+			if (event.type == SDL_EVENT_QUIT)
 				isRunning = false;
-			}
 		}
 	}
+
+	SDL_Quit();
+	return 0;
 }
